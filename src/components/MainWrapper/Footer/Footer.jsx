ca72722c9304aa12761834df7ae7136b7289d0b1
Alex Skipper <skipper@onbridge.io>
2022-02-07 18:40:24 +0300

    feat: update footer

--- src/components/MainWrapper/Footer/Footer.jsx
+++ src/components/MainWrapper/Footer/Footer.jsx
@@ -1,5 +1,17 @@
-import { Footer as FooterStyled } from './Footer.module.scss'
+import { Footer as FooterStyled, FooterIcons } from './Footer.module.scss'
 
 export function Footer() {
-  return <div className={FooterStyled}>&copy; OnBridge 2021</div>
+
+  return <div className={FooterStyled}>
+      <div>
+        <div className={FooterIcons}>
+          <a href='https://medium.com/onbridge' target="_blank"><img src="/img/medium.svg" alt="medium" /></a>
+          <a href='https://twitter.com/onbridge_io' target="_blank"><img src="/img/twitter.svg" alt="twitter" /></a>
+          <a href='https://discord.com/invite/kcNqrgS7' target="_blank"><img src="/img/discord.svg" alt="discord" /></a>
+        </div>
+        <div>
+          <p>Incubated by Binance. Supported by Polygon.</p>
+        </div>
+      </div>
+    </div>
 }
