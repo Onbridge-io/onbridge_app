a4198ca40b11d83f849596212d5d2e378795c842
OnBridge Surgeon <team@onbridge.io>
2022-03-28 15:26:05 +0300

    fix: react warnings in console

--- src/components/MainWrapper/Footer/Footer.jsx
+++ src/components/MainWrapper/Footer/Footer.jsx
@@ -2,21 +2,33 @@ import {
   Footer as FooterStyled,
   FooterIcons,
   FooterCoopyright,
-} from "./Footer.module.scss";
+} from './Footer.module.scss'
 
 export function Footer() {
   return (
     <div className={FooterStyled}>
       <div>
         <div className={FooterIcons}>
-          <a href="https://medium.com/onbridge" target="_blank">
-            <img src="/img/medium.svg" alt="medium" />
+          <a
+            href='https://medium.com/onbridge'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src='/img/medium.svg' alt='medium' />
           </a>
-          <a href="https://twitter.com/onbridge_io" target="_blank">
-            <img src="/img/twitter.svg" alt="twitter" />
+          <a
+            href='https://twitter.com/onbridge_io'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src='/img/twitter.svg' alt='twitter' />
           </a>
-          <a href="https://discord.com/invite/kcNqrgS7" target="_blank">
-            <img src="/img/discord.svg" alt="discord" />
+          <a
+            href='https://discord.com/invite/kcNqrgS7'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src='/img/discord.svg' alt='discord' />
           </a>
         </div>
         <div className={FooterCoopyright}>
@@ -24,5 +36,5 @@ export function Footer() {
         </div>
       </div>
     </div>
-  );
+  )
 }
