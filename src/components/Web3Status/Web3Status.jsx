932ce87f1a3a4b4d55af50931215487ce0a1cee5
OnBridge Surgeon <team@onbridge.io>
2022-03-29 15:14:11 +0300

    fix: incorrect network menu position when wrong network

--- src/components/Web3Status/Web3Status.jsx
+++ src/components/Web3Status/Web3Status.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
-import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
-import classnames from "classnames";
+import { useState, useEffect } from 'react'
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
+import classnames from 'classnames'
 
-import Networks from "../../networks.json";
-import { supportedChainIds } from "../../constants";
+import Networks from '../../networks.json'
+import { supportedChainIds } from '../../constants'
 import {
   Web3Status as Web3StatusStyled,
   Web3StatusAddressWrapper,
   Web3StatusAddress,
   Web3StatusHolder,
   NetworksMenu,
+  NetworksMenuImage,
+  NetworksMenuImageHidden,
   NetworksMenuTitle,
   NetworksMenuHidden,
+  NetworksMenuTranslated,
   NetworksMenuWrapper,
   NetworksMenuList,
   NetworksMenuButton,
@@ -19,114 +22,136 @@ import {
   NetworksMenuButtonContent,
   NetworksMenuMainButton,
   NetworksMenuArrow,
-} from "./Web3Status.module.scss";
-import { injected } from "../../utils/web3/connectors";
-import { useEagerConnect, useInactiveListener } from "../../utils/web3/hooks";
-import { shortenAddress, switchNetwork } from "../../utils/web3";
-import { Button } from "../";
+} from './Web3Status.module.scss'
+import { injected } from '../../utils/web3/connectors'
+import { useEagerConnect, useInactiveListener } from '../../utils/web3/hooks'
+import { shortenAddress, switchNetwork } from '../../utils/web3'
+import { Button } from '../'
 
 const connectorsByName = {
   Injected: {
-    text: "Connect",
+    text: 'Connect',
     connector: injected,
   },
-};
+}
 
 const networksLogos = {
-  42: "/img/networks-logos/mainnet.svg",
-  97: "/img/networks-logos/BSC.svg",
-  80001: "/img/networks-logos/polygon.svg",
-};
+  42: '/img/networks-logos/mainnet.svg',
+  97: '/img/networks-logos/BSC.svg',
+  80001: '/img/networks-logos/polygon.svg',
+}
 
 function NetworkSwitcher() {
-  const { error, chainId } = useWeb3React();
-  const [menuIsVisible, setMenuIsVidible] = useState(false);
-  const wrongNetwork = error instanceof UnsupportedChainIdError;
+  const { error, chainId } = useWeb3React()
+  const [menuIsVisible, setMenuIsVidible] = useState(false)
+  const wrongNetwork = error instanceof UnsupportedChainIdError
+
+  const imageClassNames = classnames(NetworksMenuImage, {
+    [NetworksMenuImageHidden]: wrongNetwork,
+  })
 
   const menuClassNames = classnames(NetworksMenu, {
     [NetworksMenuHidden]: !menuIsVisible,
-  });
+    [NetworksMenuTranslated]: wrongNetwork,
+  })
 
   const selectButtonText = wrongNetwork
-    ? "Wrong Network"
-    : Networks[chainId].name;
+    ? 'Wrong Network'
+    : Networks[chainId].name
 
   return (
     <div className={NetworksMenuWrapper}>
       <button
         className={NetworksMenuMainButton}
         onClick={() => {
-          setMenuIsVidible(!menuIsVisible);
+          setMenuIsVidible(!menuIsVisible)
         }}
       >
+        <img
+          src={networksLogos[chainId]}
+          alt='chainImage'
+          height='24'
+          width='24'
+          className={imageClassNames}
+        />
         {selectButtonText}
         <img
           className={NetworksMenuArrow}
-          src="/img/chevron-down.svg"
-          alt="Down"
+          src='/img/chevron-down.svg'
+          alt='Down'
         />
       </button>
       <div className={menuClassNames}>
         <div className={NetworksMenuTitle}>Select network</div>
 
         <div className={NetworksMenuList}>
-          {supportedChainIds.map((id) => {
+          {supportedChainIds.map(id => {
             if (chainId === id) {
               return (
                 <Button
                   key={id}
                   onClick={() => {
-                    switchNetwork(id);
+                    switchNetwork(id)
                   }}
                   className={(NetworksMenuButton, NetworksMenuButtonActive)}
                 >
                   <div className={NetworksMenuButtonContent}>
-                    <img src={networksLogos[id]} alt={Networks[id].name} />
+                    <img
+                      src={networksLogos[id]}
+                      alt={Networks[id].name}
+                      height='24'
+                      width='24'
+                    />
                     <span>{Networks[id].name}</span>
                   </div>
-                  <img src="/img/active-network-ind.svg" alt="active-network" />
+                  <img src='/img/active-network-ind.svg' alt='active-network' />
                 </Button>
-              );
+              )
             }
 
             return (
               <Button
                 key={id}
                 onClick={() => {
-                  switchNetwork(id);
+                  switchNetwork(id)
                 }}
                 className={NetworksMenuButton}
               >
                 <div className={NetworksMenuButtonContent}>
-                  <img src={networksLogos[id]} alt={Networks[id].name} />
+                  <img
+                    src={networksLogos[id]}
+                    alt={Networks[id].name}
+                    height='24'
+                    width='24'
+                  />
                   <span>{Networks[id].name}</span>
                 </div>
               </Button>
-            );
+            )
           })}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export function Web3Status({ ...props }) {
-  const { connector, account, activate, deactivate, error } = useWeb3React();
-  const wrongNetwork = error instanceof UnsupportedChainIdError;
+  const { connector, account, activate, deactivate, error } = useWeb3React()
+  const wrongNetwork = error instanceof UnsupportedChainIdError
 
   // handle logic to recognize the connector currently being activated
-  const [activatingConnector, setActivatingConnector] = useState();
+  const [activatingConnector, setActivatingConnector] = useState()
   useEffect(() => {
     if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined);
+      setActivatingConnector(undefined)
     }
-  }, [activatingConnector, connector]);
+  }, [activatingConnector, connector])
 
   // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
-  const triedEager = useEagerConnect();
+  const triedEager = useEagerConnect()
 
   // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
-  useInactiveListener(!triedEager || !!activatingConnector);
+  useInactiveListener(!triedEager || !!activatingConnector)
 
   const address = account && (
     <>
@@ -134,52 +159,52 @@ export function Web3Status({ ...props }) {
         <span className={Web3StatusAddress}>{shortenAddress(account)}</span>
       </div>
     </>
-  );
+  )
 
   const StatusContent = () => {
     const TryAgain = () => (
       <Button
         onClick={() => {
           if (connector === injected) {
-            deactivate();
+            deactivate()
           }
         }}
       >
         Try again
       </Button>
-    );
+    )
 
     if (connector === injected) {
       if (error && !wrongNetwork) {
-        return <TryAgain />;
+        return <TryAgain />
       }
 
-      return <NetworkSwitcher />;
+      return <NetworkSwitcher />
     }
 
-    return Object.keys(connectorsByName).map((name) => {
-      const currentConnector = connectorsByName[name].connector;
-      const connected = currentConnector === connector;
+    return Object.keys(connectorsByName).map(name => {
+      const currentConnector = connectorsByName[name].connector
+      const connected = currentConnector === connector
       const disabled =
-        !triedEager || !!activatingConnector || connected || !!error;
+        !triedEager || !!activatingConnector || connected || !!error
 
       const connectFunction = () => {
-        setActivatingConnector.call(this, currentConnector);
-        activate.call(this, connectorsByName[name].connector);
-      };
+        setActivatingConnector.call(this, currentConnector)
+        activate.call(this, connectorsByName[name].connector)
+      }
 
       return (
         <Button disabled={disabled} key={name} onClick={connectFunction}>
           {connectorsByName[name].text}
         </Button>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
     <div className={Web3StatusStyled} {...props}>
       <StatusContent />
       <div className={Web3StatusAddressWrapper}>{address}</div>
     </div>
-  );
+  )
 }
