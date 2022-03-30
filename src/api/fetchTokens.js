c3295439d10c95301e252373366c79ccc0c883e4
OnBridge Surgeon <team@onbridge.io>
2022-03-30 12:30:14 +0300

    feat: fetch tokens filtered by chain id #ONBD-65

--- src/api/fetchTokens.js
+++ src/api/fetchTokens.js
@@ -3,10 +3,11 @@ import axios from 'axios'
 const host = process.env.REACT_APP_API_HOST
 
 export function getTokensInfo(params = {}) {
-  const { page } = params
+  const { page, chainId } = params
+  const chainParam = chainId ? `chainId=${chainId}` : ''
   const pageParam = page ? `&page=${page}` : ''
   return axios
-    .get(`${host}/tokens/?${pageParam}`)
+    .get(`${host}/tokens/?${chainParam}${pageParam}`)
     .then((response) => {
       return response.data
     })
