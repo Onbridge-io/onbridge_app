8008ec69931c6de3e83de61400e9b457ae28a66c
OnBridge Surgeon <team@onbridge.io>
2022-03-31 12:07:51 +0300

    feat: only my tokens filter #ONBD-70

--- src/api/fetchTokens.js
+++ src/api/fetchTokens.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const host = process.env.REACT_APP_API_HOST
 
 export function getTokensInfo(params = {}) {
-  const { page, chainId } = params
-  const chainParam = chainId ? `chainId=${chainId}` : ''
+  const { page, chainId, account } = params
+  const chainParam = chainId ? `&chainId=${chainId}` : ''
   const pageParam = page ? `&page=${page}` : ''
   return axios
-    .get(`${host}/tokens/?${chainParam}${pageParam}`)
+    .get(`${host}/tokens/?owner=${account}${chainParam}${pageParam}`)
     .then((response) => {
       return response.data
     })
