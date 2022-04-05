955a86e7a0e0b74a8b14caff3395d61e6d6b40b2
OnBridge Surgeon <team@onbridge.io>
2022-04-05 16:27:05 +0300

    feat: add referral link #ONBD-87

---
+++ src/utils/hooks/useCopyToClipboard.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from 'react'
+
+function removeTextSelection() {
+  if (window.getSelection) {
+    // Chrome
+    if (window.getSelection().empty) {
+      window.getSelection().empty()
+      // Firefox
+    } else if (window.getSelection().removeAllRanges) {
+      window.getSelection().removeAllRanges()
+    }
+    // IE?
+  } else if (document.selection) {
+    document.selection.empty()
+  }
+}
+
+export function useCopyToClipboard() {
+  const ref = useRef(null)
+  const [copySuccess, setCopySuccess] = useState(false)
+
+  const copyToClipboard = () => {
+    if (!copySuccess) {
+      ref.current.select()
+      document.execCommand('copy')
+      removeTextSelection()
+      setCopySuccess(true)
+
+      setTimeout(() => {
+        setCopySuccess(false)
+      }, 1500)
+    }
+  }
+
+  return [copySuccess, copyToClipboard, ref]
+}
